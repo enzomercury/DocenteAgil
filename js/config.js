ef74707fd6a8dc226ed6a94ae596393c31825ca8
enzomercury <121519013+enzomercury@users.noreply.github.com>
2025-08-10 17:13:18 -0300

    Update and rename config.example.js to config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// /js/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAfggldAPKDhTuWD-dTZB9rHtS2qYtYwk",
+  authDomain: "docenteagil-26466.firebaseapp.com",
+  projectId: "docenteagil-26466",
+  storageBucket: "docenteagil-26466.appspot.com", // ver nota abajo
+  messagingSenderId: "484034852463",
+  appId: "1:484034852463:web:a1791ccac702b6849fff9a",
+  measurementId: "G-QGEQ3ZZN9R" // opcional; solo si vas a usar Analytics
+};
